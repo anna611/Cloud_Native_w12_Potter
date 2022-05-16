37777c2fb88c8f2b06c4347d168f7ec1658ddae8
anna611 <a0961130611@gmail.com>
2022-05-16 17:42:35 +0800

    second test <red light>

--- apps/potter/src/app/game.spec.ts
+++ apps/potter/src/app/game.spec.ts
@@ -7,7 +7,13 @@ describe('Game', () => {
 
   test('init test', () => {
     const game = new Game();
-    game.buy(1);
+    game.buy(1,0);
     expect(game.total).toBe(100);
+  });
+
+  test('second test', () => {
+    const game = new Game();
+    game.buy(1,1);
+    expect(game.total).toBe(190);
   });
 });
--- apps/potter/src/app/game.ts
+++ apps/potter/src/app/game.ts
@@ -1,9 +1,9 @@
 export class Game {
         private Total = 0;
-        buy(books: number){
-            this.Total += books * 100;
+        buy(book1: number,book2: number){
+            this.Total += book1 * 100;
         }
     get total() {
-        return this.Total
+        return this.Total;
     }
 }
