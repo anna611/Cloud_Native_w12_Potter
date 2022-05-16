47777676a83d99d3269f712848268ba010e9e308
anna611 <a0961130611@gmail.com>
2022-05-16 17:28:42 +0800

    first test <red light>

--- apps/potter/src/app/game.spec.ts
+++ apps/potter/src/app/game.spec.ts
@@ -4,4 +4,10 @@ describe('Game', () => {
   it('should create an instance', () => {
     expect(new Game()).toBeTruthy();
   });
+
+  test('init test', () => {
+    const game = new Game();
+    game.buy(1);
+    expect(game.total).toBe(100);
+  });
 });
