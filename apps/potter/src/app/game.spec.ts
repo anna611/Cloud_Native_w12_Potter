ab2fcefe62ad2522f1da5878f7e4de83beeb574f
anna611 <a0961130611@gmail.com>
2022-05-16 18:00:08 +0800

    reconstruct and thrid case<red light>

--- apps/potter/src/app/game.spec.ts
+++ apps/potter/src/app/game.spec.ts
@@ -7,13 +7,13 @@ describe('Game', () => {
 
   test('init test', () => {
     const game = new Game();
-    game.buy(1,0);
+    game.buy(1,0,0,0,0);
     expect(game.total).toBe(100);
   });
 
   test('second test', () => {
     const game = new Game();
-    game.buy(1,1);
-    expect(game.total).toBe(190);
+    game.buy(1,2,0,0,0);
+    expect(game.total).toBe(290);
   });
 });
--- apps/potter/src/app/game.ts
+++ apps/potter/src/app/game.ts
@@ -1,15 +1,19 @@
 export class Game {
         private Total = 0;
         private count = 0;
-        buy(book1: number,book2: number){
-            this.Total += (book1+book2)*100;
-            this.count = book1 + book2;
-            if(this.count > 1){
-                this.Total = this.Total * (1-(this.count-1)*0.05);
+        buy(book1: number,book2: number,book3: number,book4: number,book5: number){
+            let books: number[] = [book1,book2,book3,book4,book5];
+            this.count = this.SumBook(books);
+            this.Total += this.count * 100;
+            if( this.count > 1){
+            this.Total = this.Total*(1-(this.count-1)*0.05);
             }
 
         }
     get total() {
         return this.Total;
+    }
+    public SumBook(arr:number[]){
+        return arr.reduce((a,b)=>a+b);  
     }
 }
