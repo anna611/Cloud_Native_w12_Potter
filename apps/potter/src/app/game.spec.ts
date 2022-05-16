a288768e6d936dc6233177668207717a384e1f79
anna611 <a0961130611@gmail.com>
2022-05-16 20:52:53 +0800

    second reconstruct

--- apps/potter/src/app/game.spec.ts
+++ apps/potter/src/app/game.spec.ts
@@ -1,18 +1,26 @@
 import { Game } from './game';
 
 describe('Game', () => {
+  let game: Game;
+  beforeEach(() => {
+    game = new Game();
+  });
+
   it('should create an instance', () => {
     expect(new Game()).toBeTruthy();
   });
 
   test('init test', () => {
-    const game = new Game();
     game.buy(1,0,0,0,0);
     expect(game.total).toBe(100);
   });
 
   test('second test', () => {
-    const game = new Game();
+    game.buy(1,1,0,0,0);
+    expect(game.total).toBe(190);
+  });
+
+  test('third test', () => {
     game.buy(1,2,0,0,0);
     expect(game.total).toBe(290);
   });
--- apps/potter/src/app/game.ts
+++ apps/potter/src/app/game.ts
@@ -2,30 +2,33 @@ export class Game {
         private Total = 0;
         buy(book1: number,book2: number,book3: number,book4: number,book5: number){
             let books: number[] = [book1,book2,book3,book4,book5];
-            let discount: number[] = [0.95,0.9,0.85,0.8,0.75];
-            while(this.SumBook(books) > 0){
-                var type_count:number = 0;
-                var temp_price:number = 0;
-                for(var i = 0;i<books.length;i++) { 
-                    if(books[i] > 0){
-                        books[i]--;
-                        type_count++;
-                    }
-                 }
-                if(type_count > 1){
-                    temp_price += type_count * 100 * discount[type_count-2];
-                }
-                else{
-                    temp_price += type_count * 100; 
-                }
-                this.Total += temp_price;
-            }
-
+            this.TotalDiscount(books);
         }
+        
     get total() {
         return this.Total;
     }
     public SumBook(arr:number[]){
         return arr.reduce((a,b)=>a+b);  
+    }
+    public TotalDiscount(arr:number[]){
+        let discount: number[] = [0.95,0.9,0.85,0.8,0.75];
+        while(this.SumBook(arr) > 0){
+            var type_count:number = 0;
+            var temp_price:number = 0;
+            for(var i = 0;i<arr.length;i++) { 
+                if(arr[i] > 0){
+                    arr[i]--;
+                    type_count++;
+                }
+             }
+            if(type_count > 1){
+                temp_price += type_count * 100 * discount[type_count-2];
+            }
+            else{
+                temp_price += type_count * 100; 
+            }
+            this.Total += temp_price;
+        }
     }
 }
