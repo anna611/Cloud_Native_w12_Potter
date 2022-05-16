63a66b9df53c5ffa2ed5c718d266d1301b7d09af
anna611 <a0961130611@gmail.com>
2022-05-16 20:45:45 +0800

    thrid case<green light>

--- apps/potter/src/app/game.ts
+++ apps/potter/src/app/game.ts
@@ -1,12 +1,24 @@
 export class Game {
         private Total = 0;
-        private count = 0;
         buy(book1: number,book2: number,book3: number,book4: number,book5: number){
             let books: number[] = [book1,book2,book3,book4,book5];
-            this.count = this.SumBook(books);
-            this.Total += this.count * 100;
-            if( this.count > 1){
-            this.Total = this.Total*(1-(this.count-1)*0.05);
+            let discount: number[] = [0.95,0.9,0.85,0.8,0.75];
+            while(this.SumBook(books) > 0){
+                var type_count:number = 0;
+                var temp_price:number = 0;
+                for(var i = 0;i<books.length;i++) { 
+                    if(books[i] > 0){
+                        books[i]--;
+                        type_count++;
+                    }
+                 }
+                if(type_count > 1){
+                    temp_price += type_count * 100 * discount[type_count-2];
+                }
+                else{
+                    temp_price += type_count * 100; 
+                }
+                this.Total += temp_price;
             }
 
         }
