a9661a5afd0580379561df3997f281c73fd1acbb
anna611 <a0961130611@gmail.com>
2022-05-16 17:49:03 +0800

    second test <green light>

--- apps/potter/src/app/game.ts
+++ apps/potter/src/app/game.ts
@@ -1,7 +1,13 @@
 export class Game {
         private Total = 0;
+        private count = 0;
         buy(book1: number,book2: number){
-            this.Total += book1 * 100;
+            this.Total += (book1+book2)*100;
+            this.count = book1 + book2;
+            if(this.count > 1){
+                this.Total = this.Total * (1-(this.count-1)*0.05);
+            }
+
         }
     get total() {
         return this.Total;
